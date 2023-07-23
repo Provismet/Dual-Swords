1a1cd477f8742f8441fb2899ae11346de1aa0d0d
Provismet <17149901+Provismet@users.noreply.github.com>
2023-07-23 20:18:01 +0100

    Parry items are flipped in first and third person.

---
+++ src/main/java/com/provismet/dualswords/mixin/HeldItemFeatureRendererMixin.java
@@ -0,0 +1,36 @@
+package com.provismet.dualswords.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.provismet.dualswords.enchantments.Enchantments;
+
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.feature.FeatureRenderer;
+import net.minecraft.client.render.entity.feature.FeatureRendererContext;
+import net.minecraft.client.render.entity.feature.HeldItemFeatureRenderer;
+import net.minecraft.client.render.entity.model.EntityModel;
+import net.minecraft.client.render.model.json.ModelTransformationMode;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Arm;
+import net.minecraft.util.math.RotationAxis;
+
+@Mixin(HeldItemFeatureRenderer.class)
+public abstract class HeldItemFeatureRendererMixin<T extends LivingEntity, M extends EntityModel<T>> extends FeatureRenderer<T,M> {
+    protected HeldItemFeatureRendererMixin(FeatureRendererContext<T, M> context) {
+        super(context);
+    }
+
+    @Inject(method="renderItem", at=@At(value="INVOKE", target="Lnet/minecraft/client/render/item/HeldItemRenderer;renderItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", shift=At.Shift.BEFORE))
+    private void flipBlade (LivingEntity entity, ItemStack stack, ModelTransformationMode transformationMode, Arm arm, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo info) {
+        if (EnchantmentHelper.getLevel(Enchantments.PARRY, stack) > 0) {
+            matrices.translate(0f, -0.25f, 0.2f);
+            matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(180f));
+        }
+    }
+}
--- src/main/java/com/provismet/dualswords/mixin/HeldItemRendererMixin.java
+++ src/main/java/com/provismet/dualswords/mixin/HeldItemRendererMixin.java
@@ -1,12 +1,13 @@
 package com.provismet.dualswords.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.provismet.dualswords.DualSwordsMain;
 import com.provismet.dualswords.enchantments.Enchantments;
 
 import net.minecraft.client.network.AbstractClientPlayerEntity;
@@ -17,24 +18,55 @@ import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Arm;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.RotationAxis;
 
 @Mixin(HeldItemRenderer.class)
 public abstract class HeldItemRendererMixin {
-    public void applyParryOffset (MatrixStack matrices, float tickDelta, Arm arm, ItemStack stack) {
-        DualSwordsMain.LOGGER.info("Now parrying.");
-        matrices.translate(1f, 0f, 1f);
+    @Shadow
+    private void applyEquipOffset(MatrixStack matrices, Arm arm, float equipProgress) {}
+
+    private void applyParryOffset (MatrixStack matrices, float tickDelta, Arm arm, ItemStack stack, float equipProgress) {
+        float armMultiplier = arm == Arm.RIGHT ? -1f : 1f;
+        matrices.translate(0.5f * armMultiplier, 0f, 0.25f);
+        matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(armMultiplier * 45 * (1f - equipProgress)));
     }
 
-    @Inject(method="renderFirstPersonItem",
+    private void applyEquipOffsetUpwards (MatrixStack matrices, Arm arm, float equipProgress) {
+        float sideMultiplier = arm == Arm.RIGHT ? 1f : -1f;
+        matrices.translate(sideMultiplier * 0.56F, -0.52F + equipProgress * 0.6F, -0.72F);
+    }
+
+    @Redirect(method="renderFirstPersonItem",
         slice=@Slice(from=@At(value="INVOKE", target="Lnet/minecraft/item/ItemStack;getUseAction()Lnet/minecraft/util/UseAction;")),
         at=@At(value="INVOKE",
             target="Lnet/minecraft/client/render/item/HeldItemRenderer;applyEquipOffset(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/util/Arm;F)V",
-            ordinal=2,
-            shift=At.Shift.AFTER))
-    private void animateParry (AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand, float swingProgress, ItemStack item, float equipProgress, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo info) {
+            ordinal=2))
+    private void animateParry (HeldItemRenderer thisRenderer, MatrixStack matrices1, Arm arm, float equipProgress1, AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand, float swingProgress, ItemStack item, float equipProgress, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light) {
         if (EnchantmentHelper.getLevel(Enchantments.PARRY, item) > 0) {
+            applyEquipOffsetUpwards(matrices1, arm, equipProgress1);
+            applyParryOffset(matrices1, tickDelta, arm, item, equipProgress1);
+        }
+        else {
+            this.applyEquipOffset(matrices1, arm, equipProgress1);
+        }
+    }
+
+    @Redirect(method="renderFirstPersonItem",
+        at=@At(value="INVOKE",
+            target="Lnet/minecraft/client/render/item/HeldItemRenderer;applyEquipOffset(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/util/Arm;F)V",
+            ordinal=8))
+    private void applyUpwardsEquip (HeldItemRenderer thisRenderer, MatrixStack matrices1, Arm arm, float equipProgress1, AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand, float swingProgress, ItemStack item, float equipProgress, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light) {
+        if (EnchantmentHelper.getLevel(Enchantments.PARRY, item) > 0 && swingProgress == 0) applyEquipOffsetUpwards(matrices1, arm, equipProgress1);
+        else this.applyEquipOffset(matrices1, arm, equipProgress1);
+    }
+
+    @Inject(method="renderFirstPersonItem", at=@At(value="INVOKE", target="Lnet/minecraft/client/util/math/MatrixStack;push()V", shift=At.Shift.AFTER))
+    private void flipBlade (AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand, float swingProgress, ItemStack item, float equipProgress, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo info) {
+        if (EnchantmentHelper.getLevel(Enchantments.PARRY, item) > 0 && swingProgress == 0) {
             Arm arm = hand == Hand.MAIN_HAND ? player.getMainArm() : player.getMainArm().getOpposite();
-            applyParryOffset(matrices, tickDelta, arm, item);
+            float sideMultiplier = arm == Arm.RIGHT ? -1f : 1f;
+            matrices.translate(0.1f * sideMultiplier, -0.8f, -1.25f);
+            matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(180f));
         }
     }
 }
