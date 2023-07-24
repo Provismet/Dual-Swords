358c5b48e2098ea713df75c7d93d67dc744225cd
Provismet <17149901+Provismet@users.noreply.github.com>
2023-07-24 02:16:38 +0100

    Add offhand damage and improve sword cooldown.

--- src/main/java/com/provismet/dualswords/mixin/SwordItemMixin.java
+++ src/main/java/com/provismet/dualswords/mixin/SwordItemMixin.java
@@ -1,11 +1,23 @@
 package com.provismet.dualswords.mixin;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
 import com.provismet.dualswords.enchantments.Enchantments;
 
 import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
@@ -18,6 +30,16 @@ import net.minecraft.world.World;
 
 @Mixin(SwordItem.class)
 public abstract class SwordItemMixin extends ToolItem {
+    private static final int COOLDOWN = 30;
+    private static final int MAX_USE = 20;
+
+    @Unique
+    private Multimap<EntityAttribute, EntityAttributeModifier> dualswords_offHandAttributes = null;
+
+    @Shadow
+    @Final
+    private float attackDamage;
+
     private SwordItemMixin (ToolMaterial material, Settings settings) {
         super(material, settings);
     }
@@ -40,7 +62,7 @@ public abstract class SwordItemMixin extends ToolItem {
 
     @Override
     public int getMaxUseTime (ItemStack itemStack) {
-        if (EnchantmentHelper.getLevel(Enchantments.PARRY, itemStack) > 0) return 20;
+        if (EnchantmentHelper.getLevel(Enchantments.PARRY, itemStack) > 0) return MAX_USE;
         return super.getMaxUseTime(itemStack);
     }
 
@@ -48,9 +70,27 @@ public abstract class SwordItemMixin extends ToolItem {
     public ItemStack finishUsing (ItemStack itemStack, World world, LivingEntity user) {
         if (EnchantmentHelper.getLevel(Enchantments.PARRY, itemStack) > 0) {
             if (user instanceof PlayerEntity player) {
-                player.getItemCooldownManager().set((SwordItem)(Object)this, 30);
+                player.getItemCooldownManager().set((SwordItem)(Object)this, COOLDOWN);
             }
         }
         return itemStack;
+    }
+
+    @Override
+    public void onStoppedUsing (ItemStack itemStack, World world, LivingEntity user, int remainingUseTicks) {
+        if (EnchantmentHelper.getLevel(Enchantments.PARRY, itemStack) > 0 && user instanceof PlayerEntity player)
+            player.getItemCooldownManager().set((SwordItem)(Object)this, (int)((float)COOLDOWN * (1f - ((float)remainingUseTicks / (float)MAX_USE))));
+    }
+
+    @Inject(method="getAttributeModifiers", at=@At("HEAD"), cancellable=true)
+    private void applyOffhandMods (EquipmentSlot slot, CallbackInfoReturnable<Multimap<EntityAttribute, EntityAttributeModifier>> cir) {
+        if (slot == EquipmentSlot.OFFHAND) {
+            if (this.dualswords_offHandAttributes == null) {
+                ImmutableMultimap.Builder<EntityAttribute, EntityAttributeModifier> builder = ImmutableMultimap.builder();
+                builder.put(EntityAttributes.GENERIC_ATTACK_DAMAGE, new EntityAttributeModifier(ATTACK_DAMAGE_MODIFIER_ID, "Offhand Weapon modifier", (double)this.attackDamage / 2.5, EntityAttributeModifier.Operation.ADDITION));
+                this.dualswords_offHandAttributes = builder.build();
+            }
+            cir.setReturnValue(this.dualswords_offHandAttributes);
+        }
     }
 }
