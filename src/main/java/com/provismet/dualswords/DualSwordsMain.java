5d01769e539b118b7e640842d4f674583a1ca0df
Provismet <17149901+Provismet@users.noreply.github.com>
2023-07-24 18:05:34 +0100

    Daisho is a treasure enchantment from rare loot.

--- src/main/java/com/provismet/dualswords/DualSwordsMain.java
+++ src/main/java/com/provismet/dualswords/DualSwordsMain.java
@@ -6,6 +6,13 @@ import org.slf4j.LoggerFactory;
 import com.provismet.dualswords.enchantments.Enchantments;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.minecraft.item.Items;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTables;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.function.EnchantRandomlyLootFunction;
+import net.minecraft.loot.provider.number.BinomialLootNumberProvider;
 import net.minecraft.util.Identifier;
 
 public class DualSwordsMain implements ModInitializer {
@@ -19,6 +26,31 @@ public class DualSwordsMain implements ModInitializer {
     @Override
     public void onInitialize () {
         Enchantments.register();
+
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            if (source.isBuiltin()) {
+                if (LootTables.STRONGHOLD_LIBRARY_CHEST.equals(id)) {
+                    LootPool.Builder lootPool = LootPool.builder().rolls(BinomialLootNumberProvider.create(1, 0.333f));
+                    lootPool.with(ItemEntry.builder(Items.BOOK).apply(new EnchantRandomlyLootFunction.Builder().add(Enchantments.DAISHO)));
+                    tableBuilder.pool(lootPool);
+                }
+                else if (LootTables.WOODLAND_MANSION_CHEST.equals(id)) {
+                    LootPool.Builder lootPool = LootPool.builder().rolls(BinomialLootNumberProvider.create(1, 0.2f));
+                    lootPool.with(ItemEntry.builder(Items.BOOK).apply(new EnchantRandomlyLootFunction.Builder().add(Enchantments.DAISHO)));
+                    tableBuilder.pool(lootPool);
+                }
+                else if (LootTables.PILLAGER_OUTPOST_CHEST.equals(id)) {
+                    LootPool.Builder lootPool = LootPool.builder().rolls(BinomialLootNumberProvider.create(1, 0.15f));
+                    lootPool.with(ItemEntry.builder(Items.BOOK).apply(new EnchantRandomlyLootFunction.Builder().add(Enchantments.DAISHO)));
+                    tableBuilder.pool(lootPool);
+                }
+                else if (LootTables.HERO_OF_THE_VILLAGE_WEAPONSMITH_GIFT_GAMEPLAY.equals(id)) {
+                    LootPool.Builder lootPool = LootPool.builder().rolls(BinomialLootNumberProvider.create(1, 0.05f));
+                    lootPool.with(ItemEntry.builder(Items.BOOK).apply(new EnchantRandomlyLootFunction.Builder().add(Enchantments.DAISHO)));
+                    tableBuilder.pool(lootPool);
+                }
+            }
+        });
     }
     
 }
