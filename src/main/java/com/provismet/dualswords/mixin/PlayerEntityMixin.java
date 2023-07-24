9ae980eba3ef78e4840a7d28d08c47c8daa769f1
Provismet <17149901+Provismet@users.noreply.github.com>
2023-07-24 18:02:46 +0100

    Daisho only activates for dual wielding.

--- src/main/java/com/provismet/dualswords/mixin/PlayerEntityMixin.java
+++ src/main/java/com/provismet/dualswords/mixin/PlayerEntityMixin.java
@@ -3,7 +3,6 @@ package com.provismet.dualswords.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-
 import com.provismet.dualswords.enchantments.DaishoEnchantment;
 import com.provismet.dualswords.enchantments.Enchantments;
 
@@ -11,6 +10,7 @@ import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ToolItem;
 import net.minecraft.world.World;
 
 @Mixin(PlayerEntity.class)
@@ -21,7 +21,10 @@ public abstract class PlayerEntityMixin extends LivingEntity {
     
     @ModifyVariable(method="attack", at=@At("STORE"), ordinal=1)
     private float addDaishoDamage (float currentDamage) {
-        int DaishoLevel = EnchantmentHelper.getLevel(Enchantments.DAISHO, ((PlayerEntity)(Object)(this)).getOffHandStack());
-        return currentDamage + DaishoEnchantment.getOffhandDamage(DaishoLevel);
+        if (((PlayerEntity)(Object)this).getMainHandStack().getItem() instanceof ToolItem) { // Daisho should only work when dual-wielding.
+            int DaishoLevel = EnchantmentHelper.getLevel(Enchantments.DAISHO, ((PlayerEntity)(Object)(this)).getOffHandStack());
+            return currentDamage + DaishoEnchantment.getOffhandDamage(DaishoLevel);
+        }
+        else return currentDamage;
     }
 }
