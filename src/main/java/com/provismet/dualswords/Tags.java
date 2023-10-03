4d3aa2aed814129b73855ad53205657e3aa21575
Provismet <17149901+Provismet@users.noreply.github.com>
2023-10-03 16:46:26 +0100

    Made tag a constant.

--- src/main/java/com/provismet/dualswords/Tags.java
+++ src/main/java/com/provismet/dualswords/Tags.java
@@ -5,5 +5,5 @@ import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
 
 public class Tags {
-    public static TagKey<DamageType> BYPASSES_PARRY = TagKey.of(RegistryKeys.DAMAGE_TYPE, DualSwordsMain.identifier("bypasses_parry"));
+    public static final TagKey<DamageType> BYPASSES_PARRY = TagKey.of(RegistryKeys.DAMAGE_TYPE, DualSwordsMain.identifier("bypasses_parry"));
 }
